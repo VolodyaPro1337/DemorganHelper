786d977012038c6384ba7042aa03ee112fc54139
VolodyaPro1337 <kuleminson@gmail.com>
2026-01-13 05:00:42 +0300

    Fix 1.0.3.1: Verify API integrity

--- preload.js
+++ preload.js
@@ -12,6 +12,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
     saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
     loadSettings: () => ipcRenderer.invoke('load-settings'),
     resizeWindow: (width, height) => ipcRenderer.send('resize-window', width, height),
+    onTriggerTimer: (callback) => ipcRenderer.on('trigger-timer', (event, id) => callback(id)),
     on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
     onClickThroughState: (callback) => ipcRenderer.on('click-through-state', (event, state) => callback(state))
 });
