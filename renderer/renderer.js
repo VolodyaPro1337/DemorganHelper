be54c74dbce5153f0561163264a458d5a0c32cca
VolodyaPro1337 <kuleminson@gmail.com>
2026-01-13 05:58:42 +0300

    Fix: Explicitly declare 'settings' variable

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,5 +1,6 @@
 // Generate UUID for new timers
 let timers = {}; // Restore timers variable
+let settings = {}; // Declare settings variable
 function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
