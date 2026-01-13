d6b2a4b854f9a2bb924082a20b20a74bb4439410
VolodyaPro1337 <kuleminson@gmail.com>
2026-01-13 05:55:26 +0300

    Fix: Restore missing 'timers' variable

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,4 +1,5 @@
 // Generate UUID for new timers
+let timers = {}; // Restore timers variable
 function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
